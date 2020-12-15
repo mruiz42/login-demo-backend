bfaa6441a0cf11e5a48df67f86566af53f869626
Michael Ruiz <michael@mruiz.dev>
2020-12-15 10:38:13 -0800

    broken, trying to fix the callback for hashes

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -12,55 +12,48 @@ exports.create = (req, res) => {
     // Check if req has the content we need
     if (!req.body.username || !req.body.email || !req.body.password || !req.body.name) {
         console.log("Request cannot be empty")
+        handleResponse(req, res, 400, null, "Missing required field")
         return;
     }
     // Check if username or email exists in database
-    else if (userExists(req.body.username, req.body.email).then(res => {
-        console.log(res)
-    })) {
-        console.log("User credentials already exist");
-        return;
-    }
-    // If it does not we can proceed to adding the user to database
-    else {
-        // TODO: Salt and hash password
-        const hash = hash(req.body.password)
-
-        // TODO: validate email, user and password to conform to 255 varchar constraints
-        User.create({
-            username: req.body.username,
-            email: req.body.email,
-            name: req.body.name,
-            password: hash
-        })
-            .then(data => {
-                res.send(data)
-            })
-            .catch(err => {
-                res.status(500).send({
-                    message:
-                        err.message || "Some error occurred while creating user."
-                });
-            });
-    }
-};
-//
-// // Retrieve all Tutorials from the database.
-// exports.findAll = (req, res) => {
-//
-// };
-
-async function userExists(username, email) {
-    const user = await User.findOne({
+    User.findOne({
         where: {
             [Op.or]: [
-                {email: email},
-                {username: username}
+                {email: req.body.email},
+                {username: req.body.username}
             ]
         }
-    })
-}
+    }).then(r => {
+        if (r) {
+            handleResponse(req, res, 400, null, "User exists");
+            console.log("User credentials already exist");
+            return;
+        }
+        // If it does not we can proceed to adding the user to database
+        else {
+            // TODO: Salt and hash password
+            const hash = hash(req.body.password)
 
+            // TODO: validate email, user and password to conform to 255 varchar constraints
+            User.create({
+                username: req.body.username,
+                email: req.body.email,
+                name: req.body.name,
+                password: hash
+            })
+                .then(data => {
+                    res.send(data)
+                })
+                .catch(err => {
+                    res.status(500).send({
+                        message:
+                            err.message || "Some error occurred while creating user."
+                    });
+                });
+        }
+    });
+
+};
 // Find a single User
 exports.findOne = (req, res) => {
     if (!req.body) {
@@ -74,13 +67,7 @@ exports.findOne = (req, res) => {
                 {username: req.body.username}
             ]
         }
-    }).then(data => { res.send(data);})
-    .catch(err => {
-        res.status(500).send({
-            message:
-                err.message || "Some error occurred while retrieving username."
-        });
-    });
+    })
 };
 
 exports.authenticateCredentials = (req, res) => {
@@ -90,28 +77,34 @@ exports.authenticateCredentials = (req, res) => {
         return handleResponse(req, res, 400, null, "Username and Password required.");
       }
     else {
-        User.findOne({
-            where: { email: req.body.email, password: req.body.password }
-        }).then(data => {
-            if (data === null) {
-                console.log("not found");
-                return handleResponse(req, res, 401, null, "Unrecognized username or password.");
-            }
-            else {
-                let user = data.dataValues;
-                console.log(user);
-                if (compare(res.password, data.dataValues.password)) {
-                    let newToken = generateToken(user);
-                    console.log(newToken);
-                    res.cookie('token', newToken, {httpOnly: true});
-                    res.send(newToken);
-                    newToken.id = user.id;
-                }
-                else {
+        hash(req.body.password).then(r => {
+            User.findOne({
+                where: { email: req.body.email, password: r }
+            }).then(data => {
+                if (data === null) {
+                    console.log("not found");
                     return handleResponse(req, res, 401, null, "Unrecognized username or password.");
                 }
-            }
+                else {
+                    let user = data.dataValues;
+                    console.log(user);
+                    const result = compare(r, data.dataValues.password).then(r2 => {
+                        if (r2) {
+                            let newToken = generateToken(user);
+                            console.log(newToken);
+                            res.cookie('token', newToken, {httpOnly: true});
+                            res.send(newToken);
+                            newToken.id = user.id;
+                        }
+                        else {
+                            return handleResponse(req, res, 401, null, "Unrecognized username or password.");
+                        }
+                    })
+
+                }
+            })
         })
+
     }
 }
 
--- utils/crypto.js
+++ utils/crypto.js
@@ -3,14 +3,14 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 function hash(plaintext) {
-    bcrypt.genSalt(saltRounds, function(err, salt) {
-        bcrypt.hash(plaintext, salt, function(err, hash) {
-            return hash
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+        bcrypt.hash(plaintext, salt).then(err, hash => {
+            console.log(hash)
+            return hash;
         });
-    })
+    });
 }
-
-function compare(plaintext, hash) {
+async function compare(plaintext, hash) {
     //TODO: Not time safe comparison, maybe add a wait here?
     return bcrypt.compareSync(plaintext, hash);
 }
